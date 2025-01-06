630b58092311b5be323785644beb551b46d7048a
John Onumah <jonumah@gogoair.com>
2025-01-06 12:13:56 -0600

    adding -b flag

--- python/main.py
+++ python/main.py
@@ -2,12 +2,9 @@ import sys
 from pathlib import Path
 import argparse
 
-
-
-
-
 parser = argparse.ArgumentParser(prog="pycat")
-parser.add_argument("file",nargs="*")
+parser.add_argument("file",nargs="*", help="The file(s) that you want to print to stdout")
+parser.add_argument("-b",action="store_true", help="Print non-blank output lines, starting at 1.")
 args = parser.parse_args()
 
 
@@ -20,23 +17,22 @@ if len(sys.argv) <2:
         print("\nClosing for now.")
         sys.exit(0)
 else:
-
-    # fileNames = sys.argv[1:]
-
-
-    # for file in fileNames:
     for file in args.file:
         p = Path(file)
-
         try:
             fileName = p.resolve(strict=True)
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {file}: No such file or directory exist")
-            sys.exit(1)
-
-
+            continue
 
         with open(fileName,"r") as file:
+            if args.b:
+                line_count = 1
+                for line in file:
+                    if line == "\n":
+                        continue
+                    print(f"{line_count:>5} {line}")
+                    line_count+=1
             print(file.read())
 
 
